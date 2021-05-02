fc87aef340c0e5493c24ca8805cbe7ee94a8b6f1
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-05-02 19:50:17 +0300

    additions to html

--- dairy.js
+++ dairy.js
@@ -1,11 +1,18 @@
-// object : milk production per shed
+// object : milk production per shed in a day
 const perShed = {
 	A: 510,
     B: 308,
     C: 486,
     D: 572
 }
-// milk production per shed report
+// milk production per shed report in a day
 for (const [key, value] of Object.entries(perShed)) {
     console.log(`Your production in shed ${key} ${value} litres per day`);
   }
+// total milk production in a day
+const total = (accumulator, currentValue) => accumulator + currentValue;
+const obj = Object.values(perShed);
+
+console.log(`The total production is ${obj.reduce(total)} litres per day`);
+
+return obj.reduce(total);
