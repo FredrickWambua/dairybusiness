2cae07b25c254a277e13bcecf1e921072fc770df
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-05-03 15:24:20 +0300

    changed html,js and css content

--- dairy.js
+++ dairy.js
@@ -16,25 +16,3 @@ const obj = Object.values(perShed);
 console.log(`The total production is ${obj.reduce(total)} litres per day`);
 
 return obj.reduce(total);
-
-
-// weekly and anual procuction
-function incomeOverTime(selling_price, time){
-  switch (time) {
-      case 'weekly':
-          console.log(`Your weekly income will be Ksh ${totalProduction() * 7 * selling_price}`);
-          break;
-
-//       case 'yearly':
-//           console.log(`Your yearly income will be Ksh ${totalProduction() * 365 * selling_price}`);
-//           break;
-
-//       // case `monthly`:
-//       //     console.log(`your monthly income will be ksh ${ totalProduction() *31 * selling_price}`)
-  
-//       default:
-//           console.log(totalProduction())
-//           break;
-//   }
-//   checkLeapYear(2020, selling_price);
-// }
