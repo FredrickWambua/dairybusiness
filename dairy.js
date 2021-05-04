a7acfc91206fde8a6af6626c979593e1dbe21a29
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-05-05 02:13:49 +0300

    total production per week done

--- dairy.js
+++ dairy.js
@@ -9,10 +9,32 @@ const perShed = {
 for (const [key, value] of Object.entries(perShed)) {
     console.log(`Your production in shed ${key} ${value} litres per day`);
   }
-total milk production in a day
+// total milk production in a day
 const total = (accumulator, currentValue) => accumulator + currentValue;
 const obj = Object.values(perShed);
 
 console.log(`The total production is ${obj.reduce(total)} litres per day`);
 
 return obj.reduce(total);
+
+// calculating amount within weekly and annual production:
+function incomeOverTime(selling_price, time){
+    switch (time) {
+        case 'weekly':
+            console.log(`Your weekly income will be Ksh ${totalProduction() * 7 * selling_price}`);
+            break;
+
+        case 'yearly':
+            console.log(`Your yearly income will be Ksh ${totalProduction() * 365 * selling_price}`);
+            break;
+
+        // case `monthly`:
+        //     console.log(`your monthly income will be ksh ${ totalProduction() *31 * selling_price}`)
+    
+        default:
+            console.log(totalProduction())
+            break;
+    }
+    checkLeapYear(2020, selling_price);
+}
+// end of calculating amount within weekly and annually:
